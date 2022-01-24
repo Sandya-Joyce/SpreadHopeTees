ec73b8ed4f2e88f1c5dea0440acdb1069b57263b
Sandya Dasari <96039095+Sandya-Joyce@users.noreply.github.com>
2022-01-24 13:27:24 +0530

    Add project files.

---
+++ SpreadHopeTees/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace SpreadHopeTees.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
